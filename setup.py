31fd087b54ab1e913e02d9a4070caab7d55f05a6
Daniele Varrazzo <piro@gambitresearch.com>
2016-03-18 10:20:52 +0000

    Fixed trove classifiers
    Just an excuse to test upload with a different version number... and I
    don't know why sdist upload doesn't work.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="ltree",
-    version="0.0.1",
+    version="0.0.2",
     author="Daniele Varrazzo",
     author_email="daniele.varrazzo@gmail.com",
     description="Python wrapper for the PostgreSQL ltree data type",
@@ -21,7 +21,8 @@ setup(
     classifiers=[l.strip() for l in """
         Development Status :: 3 - Alpha
         Programming Language :: Python :: 2.7
-        Topic :: Utilities
+        Intended Audience :: Developers
         License :: OSI Approved :: BSD License
+        Topic :: Database
         """.splitlines() if l],
 )
