c22eed92a885154307b24b7641fbc7f400f7beba
Daniele Varrazzo <piro@gambitresearch.com>
2016-07-19 11:19:15 +0100

    Added django __lqmatch lookup

---
+++ ltree/django.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from django.db.models import Lookup
+from django.db.models.fields import Field
+
+
+@Field.register_lookup
+class LqueryMatch(Lookup):
+	lookup_name = 'lqmatch'
+
+	def as_postgresql(self, compiler, connection):
+		lhs, lhs_params = self.process_lhs(compiler, connection)
+		rhs, rhs_params = self.process_rhs(compiler, connection)
+		params = lhs_params + rhs_params
+		return '%s ~ %s' % (lhs, rhs), params
+
+	def as_sql(self, compiler, connection):
+		raise NotImplementedError("database vendor %s doesn't support Lquery")
