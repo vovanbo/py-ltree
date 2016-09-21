396e7f24f932c89585a376a534fed938b6654fb8
Daniele Varrazzo <piro@gambitresearch.com>
2016-09-21 19:00:17 +0100

    Detabify django module

--- ltree/django.py
+++ ltree/django.py
@@ -6,13 +6,13 @@ from django.db.models.fields import Field
 
 @Field.register_lookup
 class LqueryMatch(Lookup):
-	lookup_name = 'lqmatch'
+    lookup_name = 'lqmatch'
 
-	def as_postgresql(self, compiler, connection):
-		lhs, lhs_params = self.process_lhs(compiler, connection)
-		rhs, rhs_params = self.process_rhs(compiler, connection)
-		params = lhs_params + rhs_params
-		return '%s ~ %s' % (lhs, rhs), params
+    def as_postgresql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return '%s ~ %s' % (lhs, rhs), params
 
-	def as_sql(self, compiler, connection):
-		raise NotImplementedError("database vendor %s doesn't support Lquery")
+    def as_sql(self, compiler, connection):
+        raise NotImplementedError("database vendor %s doesn't support Lquery")
