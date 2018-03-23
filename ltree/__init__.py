4182e819ccd2fc931ef777f239c16722b3084c9e
Vladimir Bolshakov <vovanbo@gmail.com>
2018-03-23 13:47:32 +0300

    Fix import in __init__

--- ltree/__init__.py
+++ ltree/__init__.py
@@ -1,2 +1,2 @@
-from _ltree import Ltree                                # noqa
-from _lquery import Lquery, Star                        # noqa
+from ._ltree import Ltree                                # noqa
+from ._lquery import Lquery, Star                        # noqa
